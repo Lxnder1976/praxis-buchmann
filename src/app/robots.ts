6ca51b363a7a284cea492f37b6ca2a80393febf2
Alexander Buchmann <buchmann@hanseaticsoft.com>
2025-09-05 07:25:08 +0200

    Fix robots.txt domain to correct .info URL
    - Change sitemap URL from praxis-buchmann.de to praxis-buchmann.info
    - Change host from praxis-buchmann.de to praxis-buchmann.info
    - Remove placeholder comments as domain is now correctly configured
    - Ensures search engines reference the correct live domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,7 @@ export default function robots() {
         disallow: ['/api/', '/_next/'],
       },
     ],
-    sitemap: 'https://praxis-buchmann.de/sitemap.xml', // Ändern Sie dies zu Ihrer echten Domain
-    host: 'https://praxis-buchmann.de', // Ändern Sie dies zu Ihrer echten Domain
+    sitemap: 'https://praxis-buchmann.info/sitemap.xml',
+    host: 'https://praxis-buchmann.info',
   };
 }
