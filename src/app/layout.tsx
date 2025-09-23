0f81f9898762511c4e83c075ba31a84aa7f31859
Alexander Buchmann <buchmann@hanseaticsoft.com>
2025-09-23 12:02:29 +0200

    Google Tag inserted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,16 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-56ZJD6VD');`
+        }} />
+        {/* End Google Tag Manager */}
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
@@ -140,6 +150,17 @@ export default function RootLayout({
       <body
         className={`${krub.variable} ${playfairDisplay.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-56ZJD6VD"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {/* Skip Link für Barrierefreiheit */}
         <a 
           href="#main-content" 
